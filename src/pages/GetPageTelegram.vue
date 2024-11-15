c33468fd85fc74cab245208c811a1eddf60b93a2
eyhuang <enyi.huang@hand-china.com>
2024-11-15 22:17:02 +0800

    [feat]增加已领取弹窗提示

--- src/pages/GetPageTelegram.vue
+++ src/pages/GetPageTelegram.vue
@@ -1,19 +1,46 @@
 <template>
   <div class="container">
     <div class="background">
-      <img src="../asserts/getPageTelegram/background.png" alt="" style="width: 100%;">
+      <img
+        src="../asserts/getPageTelegram/background.png"
+        alt=""
+        style="width: 100%"
+      />
     </div>
-    <img src="../asserts/爱奇艺会员-logo.png" alt="" class="img-aiqiyi">
-    <img src="../asserts/中国电信.png" alt="" class="img-china-mobile">
-    <img src="../asserts/getPageTelegram/topText.png" alt="" class="img-top-text">
-    <img src="../asserts/getPageTelegram/四川电信用户专享.png" alt="" class="img-center-text">
-    <img src="../asserts/getPage/circleMatrix.png" alt="" class="img-circle-matrix">
-    <img src="../asserts/getPage/leftGift.png" alt="" class="img-left-gift">
-    <img src="../asserts/getPage/rightGift.png" alt="" class="img-right-gift">
-    <img src="../asserts/getPageTelegram/inputAfterText.png" alt="" class="img-input-after-text">
+    <img src="../asserts/爱奇艺会员-logo.png" alt="" class="img-aiqiyi" />
+    <img src="../asserts/中国电信.png" alt="" class="img-china-mobile" />
+    <img
+      src="../asserts/getPageTelegram/topText.png"
+      alt=""
+      class="img-top-text"
+    />
+    <img
+      src="../asserts/getPageTelegram/四川电信用户专享.png"
+      alt=""
+      class="img-center-text"
+    />
+    <img
+      src="../asserts/getPage/circleMatrix.png"
+      alt=""
+      class="img-circle-matrix"
+    />
+    <img src="../asserts/getPage/leftGift.png" alt="" class="img-left-gift" />
+    <img src="../asserts/getPage/rightGift.png" alt="" class="img-right-gift" />
+    <img
+      src="../asserts/getPageTelegram/inputAfterText.png"
+      alt=""
+      class="img-input-after-text"
+    />
 
     <van-cell-group inset>
-      <van-field v-model="phone" placeholder="请输入手机号码" type="tel" style="font-size: 4vw;" class="phone-field" autofocus />
+      <van-field
+        v-model="phone"
+        placeholder="请输入手机号码"
+        type="tel"
+        style="font-size: 4vw"
+        class="phone-field"
+        autofocus
+      />
     </van-cell-group>
 
     <van-button
@@ -41,31 +68,88 @@
       <DialogTextSecretTelegram />
     </van-dialog>
 
-    <van-dialog v-model:show="info1" width="60vw" :showConfirmButton="false" closeOnClickOverlay>
-      <div style="width: 60vw; text-align: center; display: flex; flex-direction: column; font-size: 4vw; padding: 5vw 0;">
+    <van-dialog
+      v-model:show="info1"
+      width="60vw"
+      :showConfirmButton="false"
+      closeOnClickOverlay
+    >
+      <div
+        style="
+          width: 60vw;
+          text-align: center;
+          display: flex;
+          flex-direction: column;
+          font-size: 4vw;
+          padding: 5vw 0;
+        "
+      >
         <div>{{ infoText }}</div>
       </div>
     </van-dialog>
 
     <!--即将领取弹窗-->
-    <van-dialog v-model:show="showPreGet" :showConfirmButton="false" closeOnClickOverlay width="80vw">
-      <div style="display: flex; flex-direction: column; width: 80vw; padding: 5vw; min-height: 30vh;">
-        <div style="display: flex;">
-          <img src="../asserts/xIcon.png" alt="" style="width: 6vw; height: 6.67vw; margin-left: 65vw;" @click="showPreGet = false">
+    <van-dialog
+      v-model:show="showPreGet"
+      :showConfirmButton="false"
+      closeOnClickOverlay
+      width="80vw"
+    >
+      <div
+        style="
+          display: flex;
+          flex-direction: column;
+          width: 80vw;
+          padding: 5vw;
+          min-height: 30vh;
+        "
+      >
+        <div style="display: flex">
+          <img
+            src="../asserts/xIcon.png"
+            alt=""
+            style="width: 6vw; height: 6.67vw; margin-left: 65vw"
+            @click="showPreGet = false"
+          />
         </div>
-        <div style="font-size: 6.25vw; font-weight: bold; margin-left: 1vw; margin-top: 3vw;">
+        <div
+          style="
+            font-size: 6.25vw;
+            font-weight: bold;
+            margin-left: 1vw;
+            margin-top: 3vw;
+          "
+        >
           即将领取爱奇艺白金会员
         </div>
-        <div style="display: flex;">
-          <img src="../asserts/getPage/aiqiyiLogo.png" alt="" style="width: 26.27vw; height: 26vw; margin-left: 22vw; margin-top: 7vw;">
+        <div style="display: flex">
+          <img
+            src="../asserts/getPage/aiqiyiLogo.png"
+            alt=""
+            style="
+              width: 26.27vw;
+              height: 26vw;
+              margin-left: 22vw;
+              margin-top: 7vw;
+            "
+          />
         </div>
-        <div style="font-size: 4vw; color: #666; margin-left: 1vw; margin-top: 7vw;">
+        <div
+          style="font-size: 4vw; color: #666; margin-left: 1vw; margin-top: 7vw"
+        >
           领取成功后，将消耗一次本月领取资格
         </div>
-        <div style="display: flex;">
+        <div style="display: flex">
           <van-button
             color="linear-gradient(to bottom, #ffb929, #fb7421)"
-            style="width: 48.4vw; height: 11.6vw; font-size: 5vw; border-radius: 5vw; margin-top: 3vw; margin-left: 11vw;"
+            style="
+              width: 48.4vw;
+              height: 11.6vw;
+              font-size: 5vw;
+              border-radius: 5vw;
+              margin-top: 3vw;
+              margin-left: 11vw;
+            "
             :loading="getButtonLoading"
             :disabled="getButtonDisabled"
             @click="handleConfirm"
@@ -77,25 +161,71 @@
     </van-dialog>
 
     <!--领取成功弹窗-->
-    <van-dialog v-model:show="showHandleSuccess" :showConfirmButton="false" closeOnClickOverlay width="80vw">
-      <div style="display: flex; flex-direction: column; width: 80vw; padding: 5vw; min-height: 30vh;">
-        <div style="display: flex;">
-          <img src="../asserts/xIcon.png" alt="" style="width: 6vw; height: 6.67vw; margin-left: 65vw;" @click="showHandleSuccess = false">
+    <van-dialog
+      v-model:show="showHandleSuccess"
+      :showConfirmButton="false"
+      closeOnClickOverlay
+      width="80vw"
+    >
+      <div
+        style="
+          display: flex;
+          flex-direction: column;
+          width: 80vw;
+          padding: 5vw;
+          min-height: 30vh;
+        "
+      >
+        <div style="display: flex">
+          <img
+            src="../asserts/xIcon.png"
+            alt=""
+            style="width: 6vw; height: 6.67vw; margin-left: 65vw"
+            @click="showHandleSuccess = false"
+          />
         </div>
-        <div style="display: flex;">
-          <img src="../asserts/successBackground.png" alt="" style="width: 33.33vw; height: 33.33vw; margin-left: 19vw;">
-          <img src="../asserts/successCheck.png" alt="" style="width: 16.8vw; height: 17.07vw; margin-left: -25vw; margin-top: 8vw;">
+        <div style="display: flex">
+          <img
+            src="../asserts/successBackground.png"
+            alt=""
+            style="width: 33.33vw; height: 33.33vw; margin-left: 19vw"
+          />
+          <img
+            src="../asserts/successCheck.png"
+            alt=""
+            style="
+              width: 16.8vw;
+              height: 17.07vw;
+              margin-left: -25vw;
+              margin-top: 8vw;
+            "
+          />
         </div>
-        <div style="font-size: 6.25vw; font-weight: bold; margin-left: 23vw; margin-top: 3vw;">
-          领取成功
+        <div
+          style="
+            font-size: 6.25vw;
+            font-weight: bold;
+            margin-left: 26vw;
+            margin-top: 3vw;
+          "
+          :style="{ marginLeft: successType === 1 ? '23vw' : '26vw' }"
+        >
+          {{ successType === 1 ?  "领取成功" : "已领取"  }}
         </div>
-        <div style="font-size: 3.25vw; color: #666; margin-left: 2vw; margin-top: 7vw;">
+        <div style="font-size: 3.25vw; color: #666; margin-left: 2vw; margin-top: 7vw;" v-if="successType === 1">
           将发送手机号码对应的爱奇艺账号请及时查收
         </div>
-        <div style="display: flex;">
+        <div style="display: flex">
           <van-button
             color="linear-gradient(to bottom, #ffb929, #fb7421)"
-            style="width: 48.4vw; height: 11.6vw; font-size: 5vw; border-radius: 5vw; margin-top: 3vw; margin-left: 11vw;"
+            style="
+              width: 48.4vw;
+              height: 11.6vw;
+              font-size: 5vw;
+              border-radius: 5vw;
+              margin-top: 3vw;
+              margin-left: 11vw;
+            "
             @click="showHandleSuccess = false"
           >
             知道了
@@ -105,22 +235,69 @@
     </van-dialog>
 
     <!--领取失败弹窗-->
-    <van-dialog v-model:show="showHandleFail" :showConfirmButton="false" closeOnClickOverlay width="80vw">
-      <div style="display: flex; flex-direction: column; width: 80vw; padding: 5vw; min-height: 30vh;">
-        <div style="display: flex;">
-          <img src="../asserts/xIcon.png" alt="" style="width: 6vw; height: 6.67vw; margin-left: 65vw;" @click="showHandleFail = false">
+    <van-dialog
+      v-model:show="showHandleFail"
+      :showConfirmButton="false"
+      closeOnClickOverlay
+      width="80vw"
+    >
+      <div
+        style="
+          display: flex;
+          flex-direction: column;
+          width: 80vw;
+          padding: 5vw;
+          min-height: 30vh;
+        "
+      >
+        <div style="display: flex">
+          <img
+            src="../asserts/xIcon.png"
+            alt=""
+            style="width: 6vw; height: 6.67vw; margin-left: 65vw"
+            @click="showHandleFail = false"
+          />
         </div>
-        <div style="display: flex;">
-          <img src="../asserts/failBackground.png" alt="" style="width: 33.33vw; height: 33.33vw; margin-left: 19vw;">
-          <img src="../asserts/failX.png" alt="" style="width: 16.8vw; height: 17.07vw; margin-left: -25vw; margin-top: 8vw;">
+        <div style="display: flex">
+          <img
+            src="../asserts/failBackground.png"
+            alt=""
+            style="width: 33.33vw; height: 33.33vw; margin-left: 19vw"
+          />
+          <img
+            src="../asserts/failX.png"
+            alt=""
+            style="
+              width: 16.8vw;
+              height: 17.07vw;
+              margin-left: -25vw;
+              margin-top: 8vw;
+            "
+          />
         </div>
-        <div style="display: flex;">
-          <img src="../asserts/failText.png" alt="" style="width: 24vw; height: 6.13vw; margin-left: 24vw; margin-top: 3vw;">
+        <div style="display: flex">
+          <img
+            src="../asserts/failText.png"
+            alt=""
+            style="
+              width: 24vw;
+              height: 6.13vw;
+              margin-left: 24vw;
+              margin-top: 3vw;
+            "
+          />
         </div>
-        <div style="display: flex;">
+        <div style="display: flex">
           <van-button
             color="linear-gradient(to bottom, #ffb929, #fb7421)"
-            style="width: 48.4vw; height: 11.6vw; font-size: 5vw; border-radius: 5vw; margin-top: 3vw; margin-left: 12vw;"
+            style="
+              width: 48.4vw;
+              height: 11.6vw;
+              font-size: 5vw;
+              border-radius: 5vw;
+              margin-top: 3vw;
+              margin-left: 12vw;
+            "
             @click="showHandleFail = false"
           >
             知道了
@@ -132,10 +309,10 @@
 </template>
 
 <script>
-import axios from 'axios';
-import DialogTextBusiness from '../components/DialogTextBusiness.vue';
-import DialogTextSecretTelegram from '../components/DialogTextSecretTelegram.vue';
-import { baseUrlTelegram, serveName } from '../utils/util.js';
+import axios from "axios";
+import DialogTextBusiness from "../components/DialogTextBusiness.vue";
+import DialogTextSecretTelegram from "../components/DialogTextSecretTelegram.vue";
+import { baseUrlTelegram, serveName } from "../utils/util.js";
 
 export default {
   name: "GetPage",
@@ -155,7 +332,8 @@ export default {
       showHandleSuccess: false,
       showHandleFail: false,
       info1: false,
-      infoText: '',
+      infoText: "",
+      successType: 1, // 1代表领取成功，2代表已领取
     };
   },
   created() {
@@ -170,7 +348,7 @@ export default {
       this.showSecretText = true;
     },
     showInfo(text) {
-      this.infoText = text
+      this.infoText = text;
       this.info1 = true;
       setTimeout(() => {
         this.info1 = false;
@@ -178,36 +356,49 @@ export default {
     },
     // 获取token并存储到本地
     getToken() {
-      axios.post(`${baseUrlTelegram}/oauth/oauth/token?client_id=client&client_secret=secret_881&grant_type=client_credentials`).then((res) => {
-        if (res?.status === 200) {
-          window.localStorage.setItem('access_token', res?.data?.access_token);
-        }
-      });
+      axios
+        .post(
+          `${baseUrlTelegram}/oauth/oauth/token?client_id=client&client_secret=secret_881&grant_type=client_credentials`
+        )
+        .then((res) => {
+          if (res?.status === 200) {
+            window.localStorage.setItem(
+              "access_token",
+              res?.data?.access_token
+            );
+          }
+        });
     },
     // 领取接口
     doGet(phone) {
-      const access_token = localStorage.getItem('access_token');
+      const access_token = localStorage.getItem("access_token");
       return new Promise((resolve) => {
         axios({
-          method: 'POST',
+          method: "POST",
           url: `${baseUrlTelegram}${serveName}/v1/0/yk-cqqy-receive-orders/action?phone=${phone}&access_token=${access_token}&agentCode=SCDX`,
-        }).then((res) => {
-          console.log('res: ', res);
-          if (res?.status === 200 && res?.data?.code === '0000') {
-            this.showHandleSuccess = true;
-          } else {
+        })
+          .then((res) => {
+            console.log("res: ", res);
+            if (res?.status === 200 && res?.data?.code === "0000") {
+              this.showHandleSuccess = true;
+              this.successType = 1;
+            } else if (res?.data?.code === "Y") {
+              this.showHandleSuccess = true;
+              this.successType = 2;
+            } else {
+              this.showHandleFail = true;
+            }
+            resolve();
+          })
+          .catch(() => {
             this.showHandleFail = true;
-          }
-          resolve();
-        }).catch(() => {
-          this.showHandleFail = true;
-          resolve();
-        });
+            resolve();
+          });
       });
     },
     getButtonClick() {
       if (!this.phone) {
-        this.showInfo('请先输入手机号！');
+        this.showInfo("请先输入手机号！");
         return;
       }
       // if (!this.checked) {
@@ -225,105 +416,108 @@ export default {
       this.getButtonLoading = false;
     },
   },
-}
+};
 </script>
 
 <style scoped lang="less">
-  .container {
-    width: 100vw;
-    position: relative;
-    .background {
-      width: 100%; position: absolute; top: 0; left: 0;
-    }
-    .img-aiqiyi {
-      position: absolute;
-      width: 21.6vw;
-      height: 2.8vw;
-      top: 4.13vw;
-      left: 3.6vw;
-    }
-    .img-china-mobile {
-      position: absolute;
-      width: 13.87vw;
-      height: 4.27vw;
-      top: 3.33vw;
-      left: 26.53vw;
-    }
-    .img-top-text {
-      position: absolute;
-      width: 88.4vw;
-      height: 22.8vw;
-      left: 5.87vw;
-      top: 13.47vw;
-    }
-    .img-center-text {
-      position: absolute;
-      width: 27.33vw;
-      height: 3.2vw;
-      left: 36.13vw;
-      top: 89.33vw;
-    }
-    .img-left-gift {
-      position: absolute;
-      width: 10.8vw;
-      height: 16.67vw;
-      left: 0vw;
-      top: 102.67vw;
-    }
-    .img-right-gift {
-      position: absolute;
-      width: 12.53vw;
-      height: 31.6vw;
-      right: 0vw;
-      top: 109.2vw;
-    }
-    .img-input-after-text {
-      position: absolute;
-      width: 76.4vw;
-      height: 6.67vw;
-      left: 11.2vw;
-      top: 123.2vw;
-    }
-    .img-circle-matrix {
-      position: absolute;
-      width: 92.8vw;
-      height: 58.13vw;
-      left: 3.6vw;
-      top: 101.73vw;
-    }
-    .phone-field {
-      position: absolute;
-      width: 69.84vw;
-      height: 10.43vw;
-      left: 15.02vw;
-      top: 109.17vw;
-      border: 0.2vw solid #666;
-      border-radius: 1vw;
-    }
-    .get-button {
-      position: absolute;
-      width: 69.84vw;
-      height: 11.6vw;
-      left: 15.02vw;
-      top: 133.73vw;
-      font-size: 6vw;
-      font-weight: bolder;
-      border-radius: 7vw;
-    }
-    .checked {
-      position: absolute;
-      width: 3.87vw;
-      height: 3.87vw;
-      left: 11.3vw;
-      top: 150.33vw;
-    }
-    .text-container {
-      position: absolute;
-      width: auto;
-      height: auto;
-      left: 16vw;
-      top: 150.33vw;
-      font-size: 3vw;
-    }
+.container {
+  width: 100vw;
+  position: relative;
+  .background {
+    width: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
   }
+  .img-aiqiyi {
+    position: absolute;
+    width: 21.6vw;
+    height: 2.8vw;
+    top: 4.13vw;
+    left: 3.6vw;
+  }
+  .img-china-mobile {
+    position: absolute;
+    width: 13.87vw;
+    height: 4.27vw;
+    top: 3.33vw;
+    left: 26.53vw;
+  }
+  .img-top-text {
+    position: absolute;
+    width: 88.4vw;
+    height: 22.8vw;
+    left: 5.87vw;
+    top: 13.47vw;
+  }
+  .img-center-text {
+    position: absolute;
+    width: 27.33vw;
+    height: 3.2vw;
+    left: 36.13vw;
+    top: 89.33vw;
+  }
+  .img-left-gift {
+    position: absolute;
+    width: 10.8vw;
+    height: 16.67vw;
+    left: 0vw;
+    top: 102.67vw;
+  }
+  .img-right-gift {
+    position: absolute;
+    width: 12.53vw;
+    height: 31.6vw;
+    right: 0vw;
+    top: 109.2vw;
+  }
+  .img-input-after-text {
+    position: absolute;
+    width: 76.4vw;
+    height: 6.67vw;
+    left: 11.2vw;
+    top: 123.2vw;
+  }
+  .img-circle-matrix {
+    position: absolute;
+    width: 92.8vw;
+    height: 58.13vw;
+    left: 3.6vw;
+    top: 101.73vw;
+  }
+  .phone-field {
+    position: absolute;
+    width: 69.84vw;
+    height: 10.43vw;
+    left: 15.02vw;
+    top: 109.17vw;
+    border: 0.2vw solid #666;
+    border-radius: 1vw;
+  }
+  .get-button {
+    position: absolute;
+    width: 69.84vw;
+    height: 11.6vw;
+    left: 15.02vw;
+    top: 133.73vw;
+    font-size: 6vw;
+    font-weight: bolder;
+    border-radius: 7vw;
+  }
+  .checked {
+    position: absolute;
+    width: 3.87vw;
+    height: 3.87vw;
+    left: 11.3vw;
+    top: 150.33vw;
+  }
+  .text-container {
+    position: absolute;
+    width: auto;
+    height: auto;
+    left: 16vw;
+    top: 150.33vw;
+    font-size: 3vw;
+  }
+}
 </style>
